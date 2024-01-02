979d6920c47f475ec908726a8c179856b7888f76
Sach Rao <rao.sachith@gmail.com>
2024-01-02 11:06:46 -0600

    updating code

--- 7kyu/two_to_one.js
+++ 7kyu/two_to_one.js
@@ -1,11 +1,5 @@
 function longest(s1, s2) {
   // your code
-  result = []
-  let concated = s1+s2
-  for (let i = 0; i<concated.length; i++) {
-    if (!(result.includes(concated[i]))) {
-      result.push(concated[i])
-    }
-  }
-  return result.sort().join('')
+  
+  return [...new Set(s1+s2)].sort().join('')
 }
