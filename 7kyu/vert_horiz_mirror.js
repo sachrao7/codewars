8a6bda97af99d327e28d3ead3ce5ba63f9fa8372
Sach Rao <rao.sachith@gmail.com>
2024-01-05 11:21:11 -0600

    update vert mirror

--- 7kyu/vert_horiz_mirror.js
+++ 7kyu/vert_horiz_mirror.js
@@ -1,17 +1,16 @@
-function vertMirror(strng) {
-    // Your code
-  //reverse each entry
-  return strng.split('\n').map( str => {
-    new_str = ''
-    for (let i=str.length - 1; i >= 0; i--) {
-      new_str += str[i]
-    }
-    return new_str
-  }).join('\n')
+function vertMirror(str) {
+    return str.split('\n')
+      .map(line => line.split('').reverse().join(''))
+      .join('\n')
 }
-function horMirror(strng) {
-  return strng.split('\n').reverse().join('\n')
+function horMirror(str) {
+    return str.split('\n')
+      .reverse()
+      .join('\n')
 }
 function oper(fct, s) {
-  return fct(s)
+    return fct(s)
 }
+
+
+
