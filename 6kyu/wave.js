d8d49dce98aaffaf2e9132ec8e2a1446d890b6d7
Sach Rao <rao.sachith@gmail.com>
2024-01-06 11:57:52 -0600

    updating code

--- 6kyu/wave.js
+++ 6kyu/wave.js
@@ -1,23 +1,11 @@
 function wave(str){
-  // Code here
-  let result = []
-  
-  for (let i =0; i<str.length; i++) {
-    let new_str = ''
-    if (str[i] != ' ') {
-        for (let j=0; j< str.length; j++) {
-          if (j==i) {
-            new_str += str[j].toUpperCase()
-          }
-          else {
-            new_str += str[j]
-          }
+    let result = [];
+    
+    str.split("").forEach((char, index) => {
+        if (char != ' ') {
+            result.push(str.slice(0, index) + char.toUpperCase() + str.slice(index + 1));
         }
-     result.push(new_str)
-    }
-
-
-
-  }
-  return result
+    });
+    
+    return result;
 }
