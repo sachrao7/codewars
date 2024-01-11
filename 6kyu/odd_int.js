105271f96db7527671c22c2ae44bba7f71ff0252
Sach Rao <rao.sachith@gmail.com>
2024-01-11 11:33:38 -0600

    alternate solution

--- 6kyu/odd_int.js
+++ 6kyu/odd_int.js
@@ -1,2 +1,22 @@
 const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
 
+
+function findOdd(A) {
+  //happy coding!
+  let counter = {}
+  
+  for (let i=0; i<A.length; i++) {
+    if (Object.keys(counter).includes(String(A[i]))) {
+      counter[A[i]] += 1
+    }
+    else {
+      counter[A[i]] = 1
+    }
+  }
+  console.log(counter)
+  for (let key in counter) {
+    if (counter[key] %2 != 0) {
+      return Number(key)
+    }
+  }
+}
