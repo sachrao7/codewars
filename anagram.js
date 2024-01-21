e35a2813b29d967c963de3f93e68dd1f0e98016b
Sach Rao <rao.sachith@gmail.com>
2024-01-21 11:52:42 -0600

    update anagram code

--- anagram.js
+++ anagram.js
@@ -4,7 +4,35 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-const ns = s.split('').sort().toString()
-const nt = t.split('').sort().toString()
-return ns==nt;
+    // // method 1
+    // // sort and see if they are equal
+    // return s.split('').sort().join('') == t.split('').sort().join('')
+
+
+
+    // method 2
+    // count each letter in the string to an counter object
+
+    if (s.length != t.length) {
+        return false
+    }
+
+    let sCounter = {}
+    let tCounter = {}
+
+    for (let i=0; i<s.length; i++) {
+        sCounter[s[i]] = (sCounter[s[i]] || 0) + 1
+        tCounter[t[i]] = (tCounter[t[i]] || 0) + 1
+    }
+
+    //check to see if counts mismatch
+    console.log(sCounter)
+    console.log(tCounter)
+    for (const key in sCounter) {
+        if (sCounter[key] != tCounter[key]) {
+            return false
+        }
+    }
+    return true
+
 };
