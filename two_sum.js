27ac99e7af58470a42144e3bfb4bcccd00cc289d
Sach Rao <rao.sachith@gmail.com>
2024-01-22 11:15:52 -0600

    leetcode

--- two_sum.js
+++ two_sum.js
@@ -1,12 +1,30 @@
-function twoSum(numbers, target) {
-    let result = []
-  
-    for (let i =0; i<numbers.length; i++) {
-      for (let j=i+1; j<numbers.length; j++) {
-         if (numbers[i] + numbers[j] == target) {
-           result.push(i, j)
-         } 
-      }
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // brute force method
+
+    // let sum = 0
+    // let result = []
+
+    // for (let i=0; i<nums.length; i++) {
+    //     for (let j=i+1; j<nums.length; j++) {
+    //         if (nums[i] + nums[j] == target) {
+    //             return [i,j]
+    //         }
+    //     }
+    // }
+
+    //hash map method
+
+    let counter = {}
+    for (let i=0; i<nums.length; i++) {
+        diff = target - nums[i]
+        if (diff in counter) {
+            return [counter[diff],i]
+        }
+        counter[nums[i]]=i
     }
-    return result
-}
+};
